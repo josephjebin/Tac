e89e4b2e07b3acb4272f09a25947b5079ad7ab87
Jebin Joseph <joseph.t.jebin@gmail.com>
2022-12-29 19:59:05 -0500

    refactored. will manually use oauth to connect to api instead of using quickstart code

--- app/src/main/java/com/example/tac/MainActivity.kt
+++ app/src/main/java/com/example/tac/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.tac
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -10,23 +9,15 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tac.ui.OneTap.OneTapSignInWithGoogle
-import com.example.tac.ui.OneTap.rememberOneTapSignInState
 import com.example.tac.ui.calendar.Calendar
 import com.example.tac.ui.task.TaskSheet
 import com.example.tac.ui.task.TaskViewModel
 import com.example.tac.ui.theme.TacTheme
 import com.google.api.services.tasks.model.TaskList
 
-enum class AuthorizationRequestCodes {
-    INSERT_AND_READ_DATA,
-    UPDATE_AND_READ_DATA,
-    DELETE_DATA
-}
 
 
 const val TAG = "Tac"
@@ -35,27 +26,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TacTheme() {
-                val state = rememberOneTapSignInState()
-                OneTapSignInWithGoogle(
-                    state = state,
-                    clientId = "518856815930-6vl6qu0gghgtp0i3gcg5mca8jc4mmau4.apps.googleusercontent.com",
-                    onTokenIdReceived = {
-                        Log.d("MainActivity", it)
-                    },
-                    onDialogDismissed = {
-                        Log.d("MainActivity", it)
-                    }
-                )
-
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(onClick = { state.open() }, enabled = !state.opened) {
-                        Text(text = "Sign in")
-                    }
-                }
             }
         }
     }
--- app/src/main/java/com/example/tac/ui/task/TasksState.kt
+++ app/src/main/java/com/example/tac/ui/task/TasksState.kt
@@ -3,4 +3,4 @@ package com.example.tac.ui.task
 import com.google.api.services.tasks.model.Task
 import com.google.api.services.tasks.model.TaskList
 
-data class TasksState(val taskLists: List<TaskList>, val tasks: List<Task>)
+data class TasksState(val taskLists: List<TaskList>, val tasks:List<Task>)
