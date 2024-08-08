3ad35a0618fa5442df84f5e92eca7c365c85e06f
Jebin Joseph <joseph.t.jebin@gmail.com>
2024-08-08 18:28:25 -0400

    centered cancel drag text

--- app/src/main/java/com/jebkit/tac/ui/layout/Tac.kt
+++ app/src/main/java/com/jebkit/tac/ui/layout/Tac.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
@@ -208,7 +209,8 @@ fun TasksAndCalendarScreen(
                 ) {
                     Text(
                         modifier = Modifier
-                            .fillMaxSize(),
+                            .fillMaxSize()
+                            .wrapContentHeight(),
                         text = "Drop here to cancel",
                         textAlign = TextAlign.Center, 
                     )
